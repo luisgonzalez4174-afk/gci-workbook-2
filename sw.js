c0e1a55e824ab2423a1b16fdae5578a991c4d4c3
LuisFranco13 <luisfranco.200312@gmail.com>
2026-06-22 22:03:28 -0600

    Add PWA support + 10 new exercises (vacation & shopping)

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'gci-wb2-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(caches.match('./index.html').then(r => r || fetch(e.request)));
+  } else {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('', {status: 408}))));
+  }
+});
